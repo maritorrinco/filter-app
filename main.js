c85014bfa7ca962352eece5002e341cdcb869c80
Maricris <maricris.orue@gmail.com>
2025-03-24 23:36:00 -0300

    add(initial project): react, tailwind, electron

---
+++ main.js
@@ -0,0 +1,21 @@
+import { app, BrowserWindow } from 'electron';
+
+let mainWindow;
+
+app.whenReady().then(() => {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+
+  mainWindow.loadURL('http://localhost:5173'); // Puerto donde corre Vite
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
